1f7106ec88ff1322a033bf71f875b69088a6d1c5
Katerina <benthebeaglecto@gmail.com>
2026-06-08 18:42:14 +0200

    gololo

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(
+export function proxy(
+
   req: NextRequest
+
 ) {
 
   // только admin
